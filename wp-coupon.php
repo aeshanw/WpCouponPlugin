d93bb8aded92553a8e9dc207bb249d53b44853f5
Aeshan Wijetunge <aeshanw@gmail.com>
2011-10-26 14:25:23 +0800

    initial plugin header added

---
+++ wp-coupon.php
@@ -0,0 +1,11 @@
+<?php
+/*
+Plugin Name: wpCoupon
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: will detect if user sign-up is reffered to by a freind and will email a coupon if so
+Version: 1.0
+Author: Aeshan Wijetunge
+Author URI: http://URI_Of_The_Plugin_Author
+License: none
+*/
+?>
